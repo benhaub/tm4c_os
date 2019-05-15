f5fb8b76e676c112809873a088ca7d97578f774b
Ben Haubrich <benhaub@benhaub.zapto.org>
2019-05-14 18:01:55 -0600

    First stable state. ROM boot loader loads stack pointer and loads pc, which goes to the reset exception, branches to main and turns the led yellow.

---
+++ OS/init.c
@@ -0,0 +1,42 @@
+/******************************************************************************
+ * Authour	:	Ben Haubrich
+ * File			:	init.c
+ * Synopsis	:	OS entry point. Boot loader loads the address from the first
+ * 						entry of the vector table, then looks for the entry point in the
+ * 						next word
+ *****************************************************************************/
+#include <tm4c123gh6pm.h>
+#include <init.h>
+
+#ifdef DONTUSE
+extern unsigned int Vectors[];
+
+/* Put the vector table into memory */
+static void putvec() {
+	unsigned int i;
+	for(i = 0; i < 16; i++) {
+		*(unsigned int *)(VTABLE_START_ADDRESS + i) = Vectors[i];
+	}
+}
+#endif
+
+int main() {
+	/* You can find which pins are LEDs by seeing the Tiva C Series LaunchPad */
+	/* Evaluation Kit User's Manual, Pg.9. The Initializaton process is found */
+	/* in the datasheet on Pg.656. */
+	//putvec();
+
+	/* Turn the light orange for now to see if we can get here */
+	SYSCTL_RCGCGPIO_R |= 0x20;
+	/* Dummy instruction to let the clock settle */
+	unsigned int dlyclk = SYSCTL_RCGCGPIO_R;
+	GPIO_PORTF_DIR_R |= (1 << 1);
+	GPIO_PORTF_DIR_R |= (1 << 3);
+	GPIO_PORTF_AFSEL_R &= ~0x01;
+	GPIO_PORTF_DEN_R |= (1 << 1);
+	GPIO_PORTF_DEN_R |= (1 << 3);
+	GPIO_PORTF_DATA_R |= (1 << 1);
+	GPIO_PORTF_DATA_R |= (1 << 3);
+	while(1);
+	return 0;
+}
