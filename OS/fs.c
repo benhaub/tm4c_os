1dc12965b837a17c7db6206b404e71071cd4eb1a
Ben Haubrich email = benhaub@gmail.com <benhaub@benhaub.zapto.org>
2019-09-11 23:28:58 -0600

    forgot to add file system files.

---
+++ OS/fs.c
@@ -0,0 +1,15 @@
+/******************************************************************************
+ * Authour	:	Ben Haubrich
+ * File			:	fs.c
+ * Synopsis	:	Implements the filesystem for tm4c_os
+ * Date			:	September 11th, 2019
+ *****************************************************************************/
+
+int init_fs() {
+/*TODO:
+ * init_fs will need to create the appropriate structs, but it will all have to
+ * be placed in flash manually. This is not a problem, we just need that
+ * functionality first.
+ */
+	return 0;
+}
