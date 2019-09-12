1f3b4b415a37e0eedbb3b6b823422ae9b351fcab
Ben Haubrich email = benhaub@gmail.com <benhaub@benhaub.zapto.org>
2019-09-11 23:42:01 -0600

    Added object file instead of header file on accident.

---
+++ OS/fs.h
@@ -0,0 +1,39 @@
+/******************************************************************************
+ * Authour	:	Ben Haubrich
+ * File			:	fs.h
+ * Synopsis	:	Implements the filesystem for tm4c_os
+ * Date			:	September 11th, 2019
+ *****************************************************************************/
+#include <types.h>
+
+#define PATHSIZE 256u
+#define NAMESIZE 16u
+/* Size of the file system in flash in bytes*/
+#define FSSIZE 32768u
+/* Block size in bytes */
+#define BSIZE 512u
+/* Maximum files that can be stored in a directory. Every block will have a */
+/* dinode, so the max number of inodes is however many can fit in what */
+/* remains. */
+#define MAXFILES ((BSIZE - sizeof(struct dinode)) / sizeof(struct inode));
+
+/* Directory index node. */
+struct dinode {
+	char path[PATHSIZE];
+	char name[NAMESIZE];
+	struct inode *files[MAXFILES];
+	word size;
+};
+
+/* File index node. */
+struct inode {
+	char name[NAMESIZE];
+	word *location;
+};
+
+/* Superblock contains general info about the entire file system. */
+struct superblock {
+	word *freeblocks[FSSIZE/BSIZE];
+	struct dinode *root;
+};
+
